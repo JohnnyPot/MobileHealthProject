6723c3c7074ef6ff2ec33042e601450d7f9df2d0
timosamp <timosamp6@gmail.com>
2020-06-19 19:03:38 +0300

    home router ready - prototype

--- MobileHealthProject/src/app/pages/home/home.page.ts
+++ MobileHealthProject/src/app/pages/home/home.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    navigate: any;
+
+    constructor() {
+        this.navigate =
+            [
+                {
+                    title: "Medicines",
+                    url: "/medicines",
+                    icon: "medkit"
+                },
+                {
+                    title: "Drug-Drug Interactions",
+                    url: "/interactions",
+                    icon: "flask"
+                },
+                {
+                    title: "Food-Drug Interactions",
+                    url: "/food",
+                    icon: "pizza"
+                },
+                // {
+                //     title: "Users",
+                //     url: "/users",
+                //     icon: "people"
+                // },
+                {
+                    title: "About",
+                    url: "/about",
+                    icon: "information-circle"
+                },
+            ]
+
+    }
+
 
 }
